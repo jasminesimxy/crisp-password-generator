cae3479b9e79d9e20a6f4926431a0c1a2950ca57
jasmine <jasminesimxy@gmail.com>
2022-12-06 13:30:03 +1100

    added variable for lower,upper,numeric and special characters

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// create variables for lowercaseChars, uppercaseChars, numericalChars, and specialChars
+var lowercaseChar= "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericalChar="0123456789";
+var specialChar= ";//"!\"#$%&'()*+,-./:;<=>?@[]^_`{}~";
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+passwordText.value = password;
+
+
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
  
+
