55fe6524d2aa6ef11b1911913a9e9d196b0929e5
jasmine <jasminesimxy@gmail.com>
2022-12-06 16:21:40 +1100

    added first prompt for password length

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("generate");
 
 // create variables for lowercaseChars, uppercaseChars, numericalChars, and specialChars
 var lowercaseChar= "abcdefghijklmnopqrstuvwxyz";
@@ -8,19 +8,41 @@ var numericalChar="0123456789";
 var specialChar= ";//"!\"#$%&'()*+,-./:;<=>?@[]^_`{}~";
 
 
+// prompt: Your password must be within the range of 8-128 characters. How many characters would you like your password to be ?
+// alert: if user enters a length <8 , then alert the user this 'Ooops.Please enter a password more than 7 characters.'
+// alert: if user enter a length number >128, then alert the user this 'Ooops, that is too many. Please enter a password less than 128 characters'
+// add confirm box pop up
+// confirm (ok) boolean (true)
+// confirm (cancel) boolean  (false)
+
+function writePassword() {
+   var passwordLength = prompt("Your password must be within the range of 8-128 characters. How many characters would you like your password to be ? ");
+
+   
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
-function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
 passwordText.value = password;
-
-
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
  
+
+
 
