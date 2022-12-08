4b674fae7e037fc1cb87c68a4ce70add760e6198
jasmine <jasminesimxy@gmail.com>
2022-12-08 14:07:19 +1100

    added the variable with the strings attached - rewrote again to make my code easier later

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
+var characterLength = 8;
+var choiceArray = [ ];
+
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseArray =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','R','S','T','U','V','W','X','Y','Z'];
+var specialCaseArray =['~','`','!','@', '#','$','%','^','&','*','(',')','-','_','+','=','{','}','[',']', '|', ':',';','"','<',',','.','>', '?','/'];
+var numericArray=['1','2','3','4','5','6','7','8','9','0']
+
 // Assignment Code
-var generateBtn = document.querySelector("generate");
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+}
+
+function generatePassword() {
+
+}
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // create variables for lowercaseChars, uppercaseChars, numericalChars, and specialChars
 var lowercaseChar= "abcdefghijklmnopqrstuvwxyz";
@@ -23,31 +73,4 @@ function writePassword() {
     alert("Ooops.Please enter a password more than 7 characters");
     return
    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Write password to the #password input
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
- 
-
-
 
